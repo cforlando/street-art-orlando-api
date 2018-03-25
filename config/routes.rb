f6592cfdc919cdea211d21f897a7080c8b9cbfb2
Axel Rivera <axel@axelrivera.me>
2018-03-25 10:41:38 -0400

    Move API resourcess

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :submissions
+
+    post 'authenticate', to: 'authentication#authenticate'
+    post 'register', to: 'users#register'
+  end
+
 end

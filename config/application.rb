8d4165579de05e64c6dd6c9e8f17622f50cbc262
Axel Rivera <axel@axelrivera.me>
2018-04-09 00:51:01 -0400

    Move lib files

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if Rails.env.development?
+  Dotenv::Railtie.load
+end
+
 module SaoWeb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

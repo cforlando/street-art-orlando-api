f17df344f72a8ad209da78f4739e27d01ff88426
Axel Rivera <axel@axelrivera.me>
2018-04-08 23:52:10 -0400

    Remove dotenv

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
 
 module SaoWeb
   class Application < Rails::Application
